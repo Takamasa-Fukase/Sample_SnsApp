1ea5cdd072b0565a9cfe013ffe06817e6846bb47
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-23 19:14:54 +0900

    横方向のグリッドの余白の設定を修正

--- Sample_SnsApp/MyPageView.swift
+++ Sample_SnsApp/MyPageView.swift
@@ -33,7 +33,7 @@ struct MyPageView: View {
                         }, label: {
                             Image(systemName: "text.badge.star")
                                 .resizable()
-                                .frame(width: 28, height: 28)
+                                .frame(width: 28, height: 24)
                         })
                     }
                     ToolbarItem(placement: .topBarTrailing) {
@@ -72,7 +72,7 @@ struct MyPageView: View {
             }
             Spacer()
         }
-        .padding(EdgeInsets(top: 0, leading: 12, bottom: 16, trailing: 12))
+        .padding(EdgeInsets(top: 0, leading: 12, bottom: 4, trailing: 12))
     }
     
     func postListGridView(geometry: GeometryProxy) -> some View {
@@ -80,24 +80,32 @@ struct MyPageView: View {
         let columns = Array(
             repeating: GridItem(
                 // 縦方向Gridの場合はこれがグリッドコンテンツの横幅の指定となる
-                // MEMO: これとは別に実際のコンテンツでもframeでwidthを指定をするとバグるので指定しないこと
-                // そもそもwidthは指定する必要がなくて、指定する場合はこれよりも小さな値にしないと表示がバグる
-                .fixed(geometry.size.width / 3 - 10)
+                // MEMO: 実際のコンテンツのwidthは指定する必要がなくて、
+                // 指定する場合はこれよりも小さな値にしないと表示がバグる
+                .fixed(
+                    // 3個並べるので2回余白が入る（2 x 2 = 4）が、
+                    // 一番右側のコンテンツが4px右側に見切れてしまうのでその分を差し引く
+                    geometry.size.width / 3 - (4 / 3)
+                ),
+                // 横方向のコンテンツ間の余白
+                spacing: 2
             ),
             // 縦方向Gridの場合は、GridItemの数が横方向のコンテンツの数になる
             count: 3
         )
         return LazyVGrid(
             columns: columns,
+            alignment: .leading,
             // 縦方向のGridに対してのspacingなので縦方向のコンテンツ間の余白
             spacing: 2
         ) {
             ForEach((0..<16), id: \.self) { _ in
-                Color(.gray)
+                Text("hoge")
                     .frame(
-//                        width: geometry.size.width / 3 - 10,
                         height: (geometry.size.width / 3) * 1.78
                     )
+                    .frame(maxWidth: .infinity)
+                    .background(.gray)
             }
         }
         .frame(maxWidth: .infinity)
