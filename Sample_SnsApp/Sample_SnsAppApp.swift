31e79db72a9144a4fc24d0f2719ec829e4ecf75b
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-23 13:20:23 +0900

    Initial Commit

---
+++ Sample_SnsApp/Sample_SnsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_SnsAppApp.swift
+//  Sample_SnsApp
+//
+//  Created by ウルトラ深瀬 on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sample_SnsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sample_SnsAppUITests/Sample_SnsAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sample_SnsAppUITestsLaunchTests.swift
+//  Sample_SnsAppUITests
+//
+//  Created by ウルトラ深瀬 on 23/11/24.
+//
+
+import XCTest
+
+final class Sample_SnsAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
