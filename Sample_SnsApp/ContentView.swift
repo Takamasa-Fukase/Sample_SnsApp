1348a68c69bda3c86a248d4add5f7598f391c4c2
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-23 15:59:50 +0900

    TabBarの装飾関連の設定を追加と、コメントを追加

--- Sample_SnsApp/ContentView.swift
+++ Sample_SnsApp/ContentView.swift
@@ -8,19 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    init() {
+        UITabBar.appearance().backgroundColor = .systemBackground
+        UITabBar.appearance().unselectedItemTintColor = .secondaryLabel
+    }
+    
     var body: some View {
         TabView {
-            HomeView()
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("Home")
-                }
-            Text("Profile")
-                .tabItem {
-                    Image(systemName: "person")
-                    Text("You")
-                }
+            Group {
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                MyPageView()
+                    .tabItem {
+                        Image(systemName: "person")
+                        Text("You")
+                    }
+                    .tint(.green)
+            }
+            // MEMO: SwiftUI専用の新しい下記のAPIを使うべきだが、非選択のTabBarのtintColorを設定するすべがまだ無く、
+            // 併用するとUITabBar.appearanceが無効になるので両方設定したい場合は、
+            // 現状はまだappearanceを使うしかない模様
+//            .toolbarBackground(.background, for: .tabBar)
+//            .toolbarBackground(.visible, for: .tabBar)
         }
+        // MEMO: UITabBar.appearance().tintColorが機能しないのでこちらを使用する必要がある
+        .tint(Color(.label))
     }
 }
 
