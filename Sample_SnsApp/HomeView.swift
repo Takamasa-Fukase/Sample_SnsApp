303b42a79c8db2df65dbcb865582d05416fdf897
Takamasa-Fukase <ultrafukase@gmail.com>
2024-11-23 17:08:38 +0900

    画面サイズの取得をGeometryReader経由に変更

--- Sample_SnsApp/HomeView.swift
+++ Sample_SnsApp/HomeView.swift
@@ -11,22 +11,27 @@ struct HomeView: View {
     let colors: [Color] = [.orange, .yellow, .green, .blue, .purple, .pink, .brown]
 
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 0) {
-                ForEach(0..<2, id: \.self) { id in
-                    verticalPostList(colors: id == 0 ? colors : colors.reversed())
+        GeometryReader(content: { geometry in
+            ScrollView(.horizontal) {
+                HStack(spacing: 0) {
+                    ForEach(0..<2, id: \.self) { id in
+                        verticalPostList(
+                            colors: id == 0 ? colors : colors.reversed(),
+                            geometry: geometry
+                        )
+                    }
                 }
             }
-        }
-        .scrollTargetBehavior(.paging)
+            .scrollTargetBehavior(.paging)
+        })
     }
     
-    func verticalPostList(colors: [Color]) -> some View {
+    func verticalPostList(colors: [Color], geometry: GeometryProxy) -> some View {
         return ScrollView(.vertical) {
             LazyVStack(spacing: 0, content: {
                 ForEach(colors, id: \.self) { color in
                     PostDetailView(backgroundColor: color)
-                        .frame(width: UIApplication.shared.screen.bounds.width)
+                        .frame(width: geometry.size.width)
                         .frame(maxHeight: .infinity)
                 }
             })
